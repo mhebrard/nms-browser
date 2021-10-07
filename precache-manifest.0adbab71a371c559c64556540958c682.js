8c90b853f428c0413004e3a8266a02806a80a9da
Maxime HEBRARD <maxime.hebrard@gmail.com>
2021-10-07 18:17:53 +0800

    Updates

--- precache-manifest.0adbab71a371c559c64556540958c682.js
+++ precache-manifest.0adbab71a371c559c64556540958c682.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "adf06bf67d11161633bcda2565f371d5",
+    "revision": "e57c168365ee40b64b9188932322c2cf",
     "url": "/nms-browser/index.html"
   },
   {
-    "revision": "a3902dd265382a10eaa3",
+    "revision": "63755d7f786391f76b26",
     "url": "/nms-browser/static/css/main.40183c98.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/nms-browser/static/js/2.9f01b239.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a3902dd265382a10eaa3",
-    "url": "/nms-browser/static/js/main.cf195c94.chunk.js"
+    "revision": "63755d7f786391f76b26",
+    "url": "/nms-browser/static/js/main.8a43428e.chunk.js"
   },
   {
     "revision": "0903f199b1a5590c6106",
